f3da7bb957a30153eb6a5c4f6f7732c00e3724a4
Osama <Eng.Osama.Fawzi@gmail.com>
2019-09-20 22:26:25 +0300

    first commit

---
+++ NousdigitalTests/NousdigitalTests.swift
@@ -0,0 +1,34 @@
+//
+//  NousdigitalTests.swift
+//  NousdigitalTests
+//
+//  Created by Osama Fawzi on 9/20/19.
+//  Copyright © 2019 Osama Fawzi. All rights reserved.
+//
+
+import XCTest
+@testable import Nousdigital
+
+class NousdigitalTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
